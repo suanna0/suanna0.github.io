1468f2ce77d724dadd76c24b948321ce445ce959
suanna0 <117539182+suanna0@users.noreply.github.com>
2025-06-19 19:15:47 -0400

    coco melon

---
+++ work/script.js
@@ -0,0 +1,53 @@
+// Scroll-to-top button
+window.onscroll = function () {
+const btn = document.getElementById("backToTop");
+if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+} else {
+    btn.style.display = "none";
+}
+};
+
+document.getElementById("backToTop").onclick = function () {
+window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+// Animate landing_desc on scroll with GSAP
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".landing_desc", {
+scrollTrigger: {
+    trigger: ".landing_desc",
+    start: "top 80%",
+},
+y: 100,
+opacity: 0,
+duration: 1,
+ease: "power4.out"
+});
+
+gsap.utils.toArray("img").forEach((imgEl) => {
+  gsap.from(imgEl, {
+    scrollTrigger: {
+      trigger: imgEl,
+      start: "top 80%",
+    },
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    ease: "power4.out"
+  });
+});
+
+gsap.utils.toArray("video").forEach((videoEl) => {
+  gsap.from(videoEl, {
+    scrollTrigger: {
+      trigger: videoEl,
+      start: "top 80%",
+    },
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    ease: "power4.out"
+  });
+});
