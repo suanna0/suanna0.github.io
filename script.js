a27ad675f59e496579d8e537ecac9a74a1ceab16
suanna0 <117539182+suanna0@users.noreply.github.com>
2025-06-26 13:59:25 -0400

    slower

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from("#Suanna", {
+  ease: "power1.out",
+  opacity: 0,
+  duration: 1,
+});
+
 gsap.to(".dark-bg", {
   ease: "power3.out",
-  delay: 1,
+  delay: 2,
   opacity: 0,
   duration: 0.5
 });
 
 gsap.from("#Suanna", {
   ease: "power3.out",
-  delay: 1.5,
+  delay: 2.5,
   scale: 2,
   y: "45vh",
 });
 
 gsap.from("#links", {
   opacity: 0,
-  delay: 1.5,
+  delay: 2.5,
   y: 200,
   duration: 0.5,
   ease: "power3.out"
@@ -25,6 +31,6 @@ gsap.from("#links", {
 gsap.to(".light-bg", {
   ease: "power3.out",
   zIndex: 1,
-  delay: 1.8,
+  delay: 2.8,
   duration: 1
 });
